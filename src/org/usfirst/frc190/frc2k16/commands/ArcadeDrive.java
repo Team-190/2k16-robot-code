44bea2bd168e90790863e09e3ba255771b73bb11
darkpositron <ericchiem@yahoo.com>
2016-02-13 15:08:01 -0500

    Added ArcadeDrive and CameraLightOn commands

---
+++ src/org/usfirst/frc190/frc2k16/commands/ArcadeDrive.java
@@ -0,0 +1,43 @@
+package org.usfirst.frc190.frc2k16.commands;
+
+import org.usfirst.frc190.frc2k16.OI;
+import org.usfirst.frc190.frc2k16.Robot;
+import org.usfirst.frc190.frc2k16.RobotMap;
+import org.usfirst.frc190.frc2k16.subsystems.DriveTrain;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class ArcadeDrive extends Command {
+
+    public ArcadeDrive() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.driveTrain);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.driveTrain.arcadeDrive(Robot.oi.getJoystick1().getY(), Robot.oi.getJoystick1().getX());
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
