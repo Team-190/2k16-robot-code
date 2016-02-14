eff81f94eb34dd6ffb67a70062909cff7d496731
Zacboy97 <boyerzr@att.net>
2016-02-14 18:31:00 -0500

    Added Toggle functionality to the Camera Light and Shifter. Added functionality to shifting. Debugged most of TestAutoNavigation.

--- src/org/usfirst/frc190/frc2k16/commands/CameraLight.java
+++ src/org/usfirst/frc190/frc2k16/commands/CameraLight.java
@@ -8,34 +8,35 @@ import edu.wpi.first.wpilibj.command.Command;
 /**
  *
  */
-public class CameraLightOff extends Command {
+public class CameraLight extends Command {
 
-    public CameraLightOff() {
+    public CameraLight() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	RobotMap.lightRelay.set(Relay.Value.kReverse);
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	RobotMap.lightRelay.set(Relay.Value.kForward);
     }
-
+    
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return true;
+        return false;
     }
 
     // Called once after isFinished returns true
     protected void end() {
-    	
+
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	RobotMap.lightRelay.set(Relay.Value.kForward);
     }
 }
